1782672a8ec1254b4bbf1443df192fb4b4580605
samiraydi <samiraydi26@gmail.com>
2019-06-25 10:04:35 +0100

    Updates

---
+++ precache-manifest.5cd082a8655d4f7ed5b5643ff9e9251d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12d4bb6a28d0bd4a21bda5a866e7ce5",
+    "url": "/docplanner-react/index.html"
+  },
+  {
+    "revision": "588e2e917f0fb907aaa4",
+    "url": "/docplanner-react/static/css/main.ddf0773e.chunk.css"
+  },
+  {
+    "revision": "edf26db9c6fc8ca6354e",
+    "url": "/docplanner-react/static/js/2.253d07d0.chunk.js"
+  },
+  {
+    "revision": "588e2e917f0fb907aaa4",
+    "url": "/docplanner-react/static/js/main.bcdf7a82.chunk.js"
+  },
+  {
+    "revision": "3e50c450e9be5833b5b0",
+    "url": "/docplanner-react/static/js/runtime~main.5671041c.js"
+  },
+  {
+    "revision": "152f007cb7e202b072209d1c95eb076b",
+    "url": "/docplanner-react/static/media/barcelona.152f007c.png"
+  },
+  {
+    "revision": "a9ff15a2b335713338f444d604c047eb",
+    "url": "/docplanner-react/static/media/curitiba.a9ff15a2.png"
+  },
+  {
+    "revision": "40d3772597883d2e2b04e59087633ba2",
+    "url": "/docplanner-react/static/media/istanbul.40d37725.png"
+  },
+  {
+    "revision": "2bfb665d5bc3cb68d78fe4011dc795e7",
+    "url": "/docplanner-react/static/media/logog.2bfb665d.svg"
+  },
+  {
+    "revision": "47ca4c93d3fb1d4d6f655284da529ef6",
+    "url": "/docplanner-react/static/media/mexico-city.47ca4c93.png"
+  },
+  {
+    "revision": "4617f89b0fe16a5ff42dc353495dbdd4",
+    "url": "/docplanner-react/static/media/rome.4617f89b.png"
+  },
+  {
+    "revision": "82d323de468a8ad9636c43e8de2a26ba",
+    "url": "/docplanner-react/static/media/screen-marketplace@2x.82d323de.png"
+  },
+  {
+    "revision": "120076c7e404fb282e53f6885f38dac7",
+    "url": "/docplanner-react/static/media/screen-saas@2x.120076c7.png"
+  },
+  {
+    "revision": "9d829d85c292405acbb619a69aa1c7fa",
+    "url": "/docplanner-react/static/media/warsaw.9d829d85.png"
+  }
+]);
